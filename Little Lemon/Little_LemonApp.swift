7ecd449e15199b7eb1e1d75bb9b803426b0822bb
Roman Sheremetiev <roman@sheremetiev.pro>
2024-03-11 22:42:32 +0100

    first commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Roman on 11.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
